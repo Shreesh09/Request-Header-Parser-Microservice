f9616c0614e03ea0ac22fc3068581857a0737970
Shreesh <shreeshnautiyal3@gmail.com>
2023-08-14 18:13:40 +0530

    finished business logic

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/whoami/', (req, res) => {
+    console.log(req.headers);
+    res.json({
+       "ipaddress": req.ip,
+       "language":req.headers['accept-language'],
+       "software": req.headers['user-agent']})
+});
+
+app.use((req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+app.listen(3000, () => console.log("Listening"));
